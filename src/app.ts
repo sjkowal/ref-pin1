69c53fcc9e8cf2e0fc8b052457875e9c03b04b24
Steven Kowal <sjkowal@users.noreply.github.com>
2021-11-17 09:12:37 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 5000;
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  res.send('This is Godzilla!');
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
